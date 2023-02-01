5518d85df787b32aeb407237de760beb11827503
KrnAlexLee <35508539+KrnAlexLee@users.noreply.github.com>
2023-01-31 20:09:00 -0700

    ATM machine like

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-print("hello world")
-print("need a job asap!")
+print("Welcome to the bank")
+print("Here you can deposit and withdrawal")
+
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        
+def deposit(self):
+    amount=float(input("Enter the amount you want to deposit: "))
+    self.balance += amount
+    print("Amount Deposited: ",amount)
+
+def withdraw(self):
+    amount = float(input("Enter the amount you wish to withdrawal: ")
+    if self.balance>=amount:
+        self.balance-=amount
+        print("You withdraw: ",amount)
+    else:
+        print("Insufficient balance")
+
+def display(self)
+print("Net Available Balance=", self.balance)
+
+s = Bank_Account()
+
+s.deposit()
+s.withdraw()
+s.display()
+
