bdbcd0f8d5f7ef7c1fe8e02c3f8c48e855af1723
Bradley Huber <bradhuber1@yahoo.com>
2023-01-31 20:37:38 -0700

    Fixed indentation so functions are part of Bank_Account class

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ class Bank_Account:
     def __init__(self):
         self.balance=0
         
-def deposit(self):
-    amount=float(input("Enter the amount you want to deposit: "))
-    self.balance += amount
-    print("Amount Deposited: ",amount)
+    def deposit(self):
+        amount=float(input("Enter the amount you want to deposit: "))
+        self.balance += amount
+        print("Amount Deposited: ",amount)
 
-def withdraw(self):
-    amount = float(input("Enter the amount you wish to withdrawal: "))
-    if self.balance>=amount:
-        self.balance-=amount
-        print("You withdraw: ",amount)
-    else:
-        print("Insufficient balance")
+    def withdraw(self):
+        amount = float(input("Enter the amount you wish to withdrawal: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("You withdraw: ",amount)
+        else:
+            print("Insufficient balance")
 
-def display(self):
-    print("Net Available Balance=", self.balance)
+    def display(self):
+        print("Net Available Balance=", self.balance)
 
 s = Bank_Account()
 
